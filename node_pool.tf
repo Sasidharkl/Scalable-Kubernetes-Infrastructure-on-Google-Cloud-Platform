0cc3d8e952f7ea2cd17bf1ad8ce34171354aa23d
KARUCHOLA LAKSHMI SASIDHAR <klakshmisasidhar@gmail.com>
2025-11-09 13:05:49 +0530

    Google Container Node Pool resource

---
+++ node_pool.tf
@@ -0,0 +1,26 @@
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = var.node_count
+
+  node_config {
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 5
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+}
